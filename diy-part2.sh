e610541c613462992451390b93722ad692d883ef
piaoyizy <925754@qq.com>
2021-06-25 08:50:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,27 +14,14 @@
 sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
 
  
-#'删除argon主题，并拉最新版'
-rm -rf package/lean/luci-theme-edge
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
-# git clone   https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+
 #'godproxy'
 rm -rf package/lean/luci-app-godproxy
 git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
 
-# theme: atmaterial	
-rm -rf package/lean/luci-theme-atmaterial	
-git clone https://github.com/sypopo/luci-theme-atmaterial.git  package/lean/luci-theme-atmaterial	
-
-cat >> .config <<EOF	
-CONFIG_PACKAGE_luci-theme-edge=y	
-CONFIG_PACKAGE_luci-theme-atmaterial=y	
-CONFIG_PACKAGE_luci-app-godproxy=y	
-
-EOF	
 
 
-#'设置默认主题'
-default_theme='edge'	
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+
+
 
